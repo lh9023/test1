61247ee2634727c0437c253aa483502e8fa63ea4
lh9023 <masterlh0923@gmail.com>
2024-03-25 16:51:01 +0800

    C++Project

---
+++ C++Project/study1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+// 必须的头文件
+#include <pthread.h>
+ 
+using namespace std;
+ 
+int main()
+{
+//	int x=101;
+//	cin>>x;
+//	int y;
+//	int i=0;
+//    // 创建一个存储字符串的向量a
+//    vector<int> obj;
+//
+//    for(int i=0;i<10;i++) // push_back(elem)在数组最后添加数据 
+//    {
+//        obj.push_back(i);
+//        cout<<obj[i]<<",";    
+//    }
+//    int j=0;
+//    while(j)
+//    {
+//    	y=x%10;
+//    	obj.push_back(y);
+//    	
+//	}
+//    return 0;
+	string str2("IVXLCDM");
+	cout<<str2[0]<<endl;
+}
